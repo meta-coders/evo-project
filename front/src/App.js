0abeb9a9a24f8d3fa21f1951b4c7fbe3071228a5
Tariod <mykhailovdmt@gmail.com>
2019-02-23 23:43:08 +0200

    feat(front): implement Sidebar component

--- front/src/App.js
+++ front/src/App.js
@@ -9,7 +9,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Switch>
           <Route exact path="/eventCard" component={EventCardContainer} />
           <Route exact path="/login" component={LoginPage} />
---
+++ front/src/components/Sidebar.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Divider';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+
+const drawerWidth = 240;
+
+const styles = {
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0
+  },
+  user: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+    minHeight: 100
+  },
+  avatar: {
+    width: 60,
+    height: 60
+  },
+  drawerPaper: {
+    width: drawerWidth
+  }
+};
+
+const Sidebar = ({ alt, avatar, children, classes, name }) => (
+  <Drawer
+    className={classes.drawer}
+    variant="permanent"
+    classes={{
+      paper: classes.drawerPaper
+    }}
+    anchor="left"
+  >
+    <div className={classes.user}>
+      <Avatar alt={alt} src={avatar} className={classes.avatar} />
+      <Typography component="h1" variant="h5">
+        {name}
+      </Typography>
+    </div>
+    <Divider />
+    <List>{children}</List>
+  </Drawer>
+);
+
+Sidebar.propTypes = {
+  alt: PropTypes.string,
+  avatar: PropTypes.string,
+  classes: PropTypes.object.isRequired,
+  name: PropTypes.string,
+  children: PropTypes.node
+};
+
+export default withStyles(styles)(Sidebar);
